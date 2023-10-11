7e7ec72f6969d096637e35c781b0049b672a26e4
Ben Priborsky <ben.priborsky@titanom.com>
2023-10-11 15:33:55 +0200

    feat: added title

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import  TumIcon  from './components/icons/tum'
+import TumIcon from './components/icons/tum'
 
 const config: DocsThemeConfig = {
   logo: <strong>Aerospace docs 🚀✈️</strong>,
@@ -9,8 +9,14 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/BenPriborsky/aerospace-docs/tree/main',
   footer: {
-    text: <div style={{display: "inline-block"}}><TumIcon size={25} /><strong>Areospace</strong></div>,
+    text: <div style={{ display: "inline-block" }}><TumIcon size={25} /><strong>Areospace</strong></div>,
   },
 }
 
-export default config
+export default {
+  config, useNextSeoProps() {
+    return {
+      titleTemplate: '%s - Aerospace'
+    }
+  }
+}
